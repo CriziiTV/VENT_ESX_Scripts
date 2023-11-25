5a9ba4a3be83ea96b0cee80bcb0562d22f82b4a8
Crizii <135024567+CriziiTV@users.noreply.github.com>
2023-11-25 23:52:04 +0100

    Create de.lua
    German Language File

---
+++ esx_advancedholdup/locales/de.lua
@@ -0,0 +1,25 @@
+Locales['de'] = {
+	-- Main
+	['press_to_rob'] = 'Drücken Sie ~INPUT_CONTEXT~ um den ~o~~h~Raub zu beginnen!~s~',
+
+	-- Robbery Notifications
+	['rob_in_prog'] = '~r~Ein Raub ist bereits im Gange!',
+	['rob_started'] = 'Sie haben einen Raub begonnen, ~o~GEHEN SIE NICHT ZU WEIT WEG!!!~s~',
+	['rob_progress'] = 'Raub im Gange: ~r~%s~s~ Sekunden verbleibend.',
+	['rob_complete'] = '~g~Raub beendet, Geld aufgehoben.~s~~n~~p~~h~Lauf kleines Mädchen!~s~',
+	['rob_cancel'] = '~r~Sie sind weggelaufen, bevor Sie den Safe öffnen konnten...~s~',
+	['rob_already'] = '~o~Dieser Laden wurde bereits ausgeraubt. ~s~Bitte warten Sie ~r~%s~s~ Sekunden, bevor Sie ihn erneut ausrauben.',
+	['rob_vehicle'] = '~r~Sie können nicht rauben, während Sie in einem Fahrzeug sind...~s~',
+	['rob_required'] = 'Es werden mindestens ~b~%s~s~ Polizeibeamte benötigt, um diesen Ort auszurauben.',
+	['rob_cooldown'] = 'Es gibt derzeit eine Abkühlzeit oder ein Raub ist bereits im Gange. Kommen Sie später wieder.',
+	['cant_job'] = 'Sie haben nicht den richtigen Job, um Raubüberfälle zu starten!',
+
+	-- Police Alerts
+	['police_alert'] = 'Polizeialarm',
+	['police_alert_alarm'] = '~b~10-32~s~ : Ein lauter Alarm wurde bei ~y~%s~s~ ausgelöst!',
+	['police_alert_alarm_subject'] = 'Raub im Gange!',
+	['police_alert_complete'] = '~b~10-32~s~ : Eine Person wurde gesehen, wie sie den Ort mit den Händen ~g~voll von etwas~s~ bei ~y~%s~s~ verließ!',
+	['police_alert_complete_subject'] = 'Raub beendet!',
+	['police_alert_cancel'] = '~b~10-32~s~ : Eine Person wurde gesehen, wie sie den Ort ~g~mit leeren Händen~s~ bei ~y~%s~s~ verließ!',
+	['police_alert_cancel_subject'] = 'Raub abgebrochen!',
+}
